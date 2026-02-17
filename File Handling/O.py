e1de2cb1b1fccf28263fe68142a89453927e1072
crishnaverma <crishnaverma@gmail.com>
2026-02-17 23:35:41 +0530

    delete file handling

--- File Handling/O.py
+++
@@ -1,9 +0,0 @@
-with open("File Handling/file.txt","r") as f:
-    print(f.read())
-    
-    # (to enter the name)
-    # for i in range(3):
-    #     name = input("Enter name: ")
-    #     f.write(name + "\n")
-
-
